c5271b55b2c7bcda59def883fa00147288e252a5
Nephe <hetier.guillaume@gmail.com>
2016-11-05 16:07:04 -0400

    Make the message queue non blocking
    - improve error message
    - define test init value
    - injection thread now consume all message of their list and then,
      the injection.

--- INF6600_td4.cc
+++ INF6600_td4.cc
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <mqueue.h>
 #include <string.h>
+#include <errno.h>
 
 #define MSG_SIZE 4096
 
@@ -15,17 +16,12 @@ mqd_t qr_insuline;
 class Patient {
     public:
 
-        Patient() : glucose(0), insuline(0) {}
-
-        pthread_mutex_t m_glucose;
-        pthread_mutex_t m_insuline;
-        int glucose;
-        int insuline;
+        Patient() : glucose(100), insuline(0) {}
 
         static const double glycemia_ref = 120;
         static const double glycemia_crit = 60;
-        static const int glucose_step = 1;
-        static const int insuline_step = 3;
+        static const int glucose_step = 10;
+        static const int insuline_step = 30;
         static const int Kg = 1;
         static const int Ki = 1;
 
@@ -47,17 +43,23 @@ class Patient {
 
         void injectInsuline() {
             pthread_mutex_lock(&m_insuline);
-            glucose += glucose_step;
+            insuline += insuline_step;
             pthread_mutex_unlock(&m_insuline);
         }
+
+        pthread_mutex_t m_glucose;
+        pthread_mutex_t m_insuline;
+        int glucose;
+        int insuline;
 };
 
-enum Message {STOP, START};
+enum Message {STOP, START, NONE};
 
 void *t_patient(void *args) {
     Patient *patient = (Patient *)args;
 
     for (int i = 0; i < 10; i++) {
+        sleep(1);
         double glycemia = patient->computeGlycemia();
         std::cout <<  "Glycemia : " << glycemia << std::endl;
 
@@ -69,7 +71,7 @@ void *t_patient(void *args) {
                 std::cout << "Error sending glucose msg" << std::endl;
             if(mq_send(qw_insuline, (const char *) &msg_insuline,
                         sizeof(msg_insuline), 0) < 0)
-                std::cout << "Error sending glycemia  msg" << std::endl;
+                std::cout << "Error sending insuline msg" << std::endl;
         } else if (glycemia >= patient->glycemia_ref) {
             Message msg_glucose = STOP;
             Message msg_insuline = START;
@@ -78,7 +80,7 @@ void *t_patient(void *args) {
                 std::cout << "Error sending glucose msg" << std::endl;
             if(mq_send(qw_insuline, (const char *) &msg_insuline,
                         sizeof(msg_insuline), 0) < 0)
-                std::cout << "Error sending glycemia  msg" << std::endl;
+                std::cout << "Error sending insuline  msg" << std::endl;
         }
     }
 
@@ -90,11 +92,27 @@ void *t_glucose(void *args) {
     bool isInjecting = false;
 
     for (int i = 0; i < 10; i++) {
-        Message msg;
+        sleep(1);
+        Message msg = NONE;
+        mq_attr attr, old_attr;
+        mq_getattr(qr_glucose, &attr);
+        if (attr.mq_curmsgs != 0) {
+            // First set the queue to not block any calls
+            attr.mq_flags = O_NONBLOCK;
+            mq_setattr(qr_insuline, &attr, &old_attr);
+            // Now consume all of the messages. Only the last one is usefull
+            while (mq_receive(qr_glucose, (char *) &msg, MSG_SIZE, NULL) != -1) {}
+            if (errno == EAGAIN)
+                std::cout << "No message is glucose queue" << std::endl;
+            else
+                std::cout << "Error receiving glucose msg" << std::endl;
+
+            // Now restore the attributes
+            mq_setattr (qr_glucose, &old_attr, NULL);
+        }
         int byte_read = mq_receive(qr_glucose, (char *) &msg, MSG_SIZE, NULL);
-        if (byte_read < 0) {
-            std::cout << "Error receiving glucose msg" << std::endl;
-        } else if (msg== START) {
+
+        if (msg== START) {
             isInjecting = true;
             std::cout << "Glucose : Msg start" << std::endl;
         } else if (msg == STOP) {
@@ -104,20 +122,38 @@ void *t_glucose(void *args) {
 
         if (isInjecting)
             patient->injectGlucose();
+            std::cout << "Glucose : Injection" << std::endl;
     }
     pthread_exit(NULL);
 }
 
 void *t_insuline(void *args) {
     Patient *patient = (Patient *)args;
-    bool isInjecting = false;
+    bool isInjecting = true;
 
     for(int i = 0; i < 10; i++) {
-        Message msg;
-        int byte_read = mq_receive(qr_insuline, (char *) &msg, MSG_SIZE, NULL);
-        if (byte_read < 0) {
-            std::cout << "Error receiving insuline msg" << std::endl;
-        } else if (msg == START) {
+        sleep(1);
+        Message msg = NONE;
+
+        mq_attr attr, old_attr;
+        mq_getattr (qr_insuline, &attr);
+        if (attr.mq_curmsgs != 0) {
+            // There are some messages on this queue....eat em
+            // First set the queue to not block any calls
+            attr.mq_flags = O_NONBLOCK;
+            mq_setattr(qr_insuline, &attr, &old_attr);
+            // Now eat all of the messages
+            while (mq_receive(qr_insuline, (char *) &msg, MSG_SIZE, NULL) != -1) {}
+            if (errno == EAGAIN)
+                std::cout << "No message is insuline queue" << std::endl;
+            else
+                std::cout << "Error receiving insuline msg" << std::endl;
+
+            // Now restore the attributes
+            mq_setattr (qr_insuline, &old_attr, NULL);
+        }
+
+        if (msg == START) {
             isInjecting = true;
             std::cout << "Insuline : Msg start" << std::endl;
         } else if (msg == STOP) {
@@ -125,8 +161,10 @@ void *t_insuline(void *args) {
             std::cout << "Insuline : Msg stop" << std::endl;
         }
 
-        if (isInjecting)
+        if (isInjecting) {
             patient->injectInsuline();
+            std::cout << "Insuline : Injection" << std::endl;
+        }
     }
     pthread_exit(NULL);
 }
@@ -154,21 +192,21 @@ int main(int argc, char **argv) {
     qr_glucose = mq_open("q_glucose",
             O_CREAT | O_RDONLY, S_IWUSR | S_IRUSR, &attr_glucose);
     if(qr_glucose == (mqd_t)0)
-        std::cout << "ERROR1" << std::endl;
+        std::cout << "Error creating `qr_glucose`" << std::endl;
 
     qw_glucose= mq_open("q_glucose", O_WRONLY);
     if(qw_glucose == (mqd_t)0)
-        std::cout << "ERROR2" << std::endl;
+        std::cout << "Error creating `qw_glucose`" << std::endl;
 
     mq_unlink("q_insuline");
     qr_insuline = mq_open("q_insuline",
             O_CREAT | O_RDONLY, S_IWUSR | S_IRUSR, &attr_insuline);
     if(qr_insuline == (mqd_t)0)
-        std::cout << "ERROR1" << std::endl;
+        std::cout << "Error creating `qr_insuline`" << std::endl;
 
     qw_insuline = mq_open("q_insuline", O_WRONLY);
     if(qw_insuline == (mqd_t)0)
-        std::cout << "ERROR2" << std::endl;
+        std::cout << "Error creating `qw_insuline`"<< std::endl;
 
     pthread_t th_glucose;
     pthread_create(&th_glucose, NULL, t_glucose, &patient);
@@ -180,6 +218,7 @@ int main(int argc, char **argv) {
     pthread_create(&th_patient, NULL, t_patient, &patient);
 
     pthread_join(th_glucose, NULL);
+    pthread_join(th_insuline, NULL);
     pthread_join(th_patient, NULL);
 
     mq_close(qr_glucose);
